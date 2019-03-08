b4ea748f339b645a539746b190a47bd5f0493598
Eduard Ramirez <me@tantaroth.com>
2019-03-07 23:35:44 -0500

    Admin api functions, firestore & storage
    Admin api functions firestore trigger and upload file with firebase cloud storage admin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,35 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const bucket = admin.storage().bucket("gs://ng-lab.appspot.com");
+
+export const triggers = functions.firestore.document("triggers/{triggerId}").onCreate(snap => {
+  const data = snap.data();
+
+  if (data) {
+    const pathLog = `upload/${data.uuid}/${data.uuid}.log`;
+
+    bucket.file(pathLog).save(
+      Buffer.from(JSON.stringify(data)),
+      {
+        metadata: { contentType: "text/html" }
+      },
+      error => {
+        if (error) {
+          return;
+        }
+
+        data.output = pathLog;
+
+        const afs = admin.firestore();
+        afs
+          .doc("triggers/" + data.uuid)
+          .set(data)
+          .then(() => console.log("Good!"))
+          .catch(console.error);
+      }
+    );
+  }
+});
